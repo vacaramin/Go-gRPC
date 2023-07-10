a1bbe284b5ce41be7e41cd1dc29515f8ce0f59f5
Waqar Amin <vacaramin86@gmail.com>
2023-07-11 03:24:39 +0500

    server hello part complete

--- server/main.go
+++ server/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net"
 
+	pb "github.com/vacaramin/Go-gRPC/proto"
 	"google.golang.org/grpc"
 )
 
@@ -11,13 +12,17 @@ const (
 	port = ":8080"
 )
 
+type helloServer struct {
+	pb.GreetServiceServer
+}
+
 func main() {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to start the server %v", err)
 	}
 	grpcServer := grpc.NewServer()
-	pb.RegisterGreetServiceServer(gprcServer, &helloServer{})
+	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
 	log.Printf("Server started at %v", lis.Addr())
 
 	if err := grpcServer.Serve((lis)); err != nil {
