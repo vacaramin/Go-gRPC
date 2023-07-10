459631062414d5f08f1d9c382a461ac9492f4c27
Waqar Amin <vacaramin86@gmail.com>
2023-07-11 03:30:54 +0500

    basic client functionality ready

--- client/main.go
+++ client/main.go
@@ -1,1 +1,25 @@
-package client
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Did not connect %v", err)
+	}
+	defer conn.Close()
+	//client := pb.NewGreetServiceClient(conn)
+
+	// names := &pb.NamesList{"
+	// Names: [string]{"Waqar", "Alice", "Bob"}"}
+
+}
