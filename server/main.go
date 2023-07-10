f3b922eee42d0ee73cb795786bf2893b2b70170f
Waqar Amin <vacaramin86@gmail.com>
2023-07-11 02:50:56 +0500

    initalizing the grpc server side

--- server/main.go
+++ server/main.go
@@ -1,1 +1,26 @@
-package server
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to start the server %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreetServiceServer(gprcServer, &helloServer{})
+	log.Printf("Server started at %v", lis.Addr())
+
+	if err := grpcServer.Serve((lis)); err != nil {
+		log.Fatalf("Failed to Start: %v", err)
+	}
+}
