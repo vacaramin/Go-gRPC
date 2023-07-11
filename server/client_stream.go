85a482c1eb98ccfc75da53bad15e061fffb15a0f
Waqar Amin <vacaramin86@gmail.com>
2023-07-11 05:36:28 +0500

    client stream on server implemented

---
+++ server/client_stream.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"log"
+
+	pb "github.com/vacaramin/Go-gRPC/proto"
+)
+
+func (s *helloServer) SayhelloClientStreaming(stream pb.GreetService_SayhelloClientStreamingServer) error {
+	var messages []string
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.MessagesList{Name: messages})
+		}
+		if err != nil {
+			return err
+		}
+		log.Printf(("Got request with name:%v"), req.Message)
+		messages = append(messages, "hello ", req.Message)
+	}
+}
